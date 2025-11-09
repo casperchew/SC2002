24f48b6ba3cb9255bd9475691742d61778d95b3e
casperchew <94462480+casperchew@users.noreply.github.com>
2025-11-10 07:23:30 +0800

    Delete docs directory

--- docs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"model.internship","l":"InternshipApplication"},{"p":"model.internship","l":"InternshipOpportunity"}];updateSearchResults();
